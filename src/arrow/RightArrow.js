8970c82cd8db8af09d16a8f771cf9995ae3182af
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-01-13 16:30:54 +0700

    feat: add arrows

---
+++ src/arrow/RightArrow.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import PropTypes from "prop-types";
+
+class RightArrow extends React.Component {
+
+    static propTypes = {
+        title: PropTypes.string,
+        CenterComponent: PropTypes.node,
+    }
+
+    static defaultProps = {
+        title: "Right",
+    }
+
+    render() {
+
+        const {
+            title,
+            CenterComponent,
+        } = this.props;
+
+        const comps = CenterComponent ? CenterComponent : (
+            <Button
+                title={title}    
+            />
+        );
+
+        return (
+            <View style={styles.container}>
+                {comps}
+            </View>
+        )
+    }
+}
+
+export default RightArrow;
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        zIndex: 1,
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Carousel from './carousel/Carousel';
 import Pagination from './pagination/Pagination';
+import LeftArrow from './pagination/LeftArrow';
+import RightArrow from './pagination/RightArrow';
 import ParallaxImage from './parallaximage/ParallaxImage';
 import { getInputRangeFromIndexes } from './utils/animations';
 
-export { Carousel as default, Pagination, ParallaxImage, getInputRangeFromIndexes };
+export { 
+    Carousel as default, 
+    Pagination, 
+    LeftArrow,
+    RightArrow,
+    ParallaxImage, 
+    getInputRangeFromIndexes,
+};
